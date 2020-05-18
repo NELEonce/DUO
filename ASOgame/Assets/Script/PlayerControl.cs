32099ea422505cd03191c0365fe20292f158076c
藤本翔大 <1816232@s.asojuku.ac.jp>
2020-05-18 13:23:37 +0900

    移動アニメーションのスクリプト

--- ASOgame/Assets/Script/PlayerControl.cs
+++ ASOgame/Assets/Script/PlayerControl.cs
@@ -8,15 +8,21 @@ public class PlayerControl : MonoBehaviour
     private CharacterController characterController;    //CharacterController型の変数
     private Vector3 Velocity;   //キャラクターコントローラーを動かすためのVector3型の変数
     public float JumpPowewr;    //ジャンプ
-    public Transform verRot;    //縦の視点移動の変数（カメラに合わせる）
+    public Transform verRot;    //縦の視移c動の変数（カメラに合わせる）
     public Transform horRot;    //横の視点移動の変数（プレイヤーに合わせる）
     public float MoveSpeed;     //移動速度
-
+    private Animator anim;
+    private Ray ray;            //Ray型の変数
+    public GameObject Explosion;//敵を倒したときに出現させる爆発
+    private int distance = 10000;//Rayを飛ばす距離
+    private RaycastHit hit;     //Rayが何かに当たった時の情報
+    public GameObject Muzzle;   //Rayを発射する場所
 
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
     }
 
 
@@ -28,53 +34,51 @@ public class PlayerControl : MonoBehaviour
         verRot.transform.Rotate(-Y_Rotation * 2, 0, 0);    //カメラのX軸の回転をY_Rotationに合わせる
 
 
-        /*
-        if(Input.GetKey(KeyCode.W)) //Wキーがおされたら
-        {
-            characterController.Move(this.gameObject.transform.forward * MoveSpeed * Time.deltaTime);   //前にMoveSpeed * Time.deltaTimeだけ動かす
-        }
-
-        if (Input.GetKey(KeyCode.W)) //Sキーがおされたら
-        {
-            characterController.Move(this.gameObject.transform.forward * -1f * MoveSpeed * Time.deltaTime); //後ろにMoveSpeed * Time.deltaTimeだけ動かす
-        }
-
-        if (Input.GetKey(KeyCode.W)) //Aキーがおされたら
-        {
-            characterController.Move(this.gameObject.transform.right * -1 * MoveSpeed * Time.deltaTime);   //左ににMoveSpeed * Time.deltaTimeだけ動かす
-        }
-
-        if (Input.GetKey(KeyCode.W)) //Dキーがおされたら
-        {
-            characterController.Move(this.gameObject.transform.right * MoveSpeed * Time.deltaTime);   //右にMoveSpeed * Time.deltaTimeだけ動かす
-        }
-        */
         
 
         if (Input.GetKey(KeyCode.W))//Wキーがおされたら 
         {
             //前方にMoveSpeed＊Time.deltaTimeだけ動かす
             characterController.Move(this.gameObject.transform.forward * MoveSpeed * Time.deltaTime);
+            anim.SetBool("Run", true);
+        }
+        else if(Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetBool("Run", false);
         }
 
         if (Input.GetKey(KeyCode.S))//①Sキーがおされたら
         {
             //後方にMoveSpeed＊Time.deltaTimeだけ動かす
             characterController.Move(this.gameObject.transform.forward * -1f * MoveSpeed * Time.deltaTime);
+            anim.SetBool("Run", true);
+        }
+        else if(Input.GetKeyUp(KeyCode.S))
+        {
+            anim.SetBool("Run", false);
         }
 
         if (Input.GetKey(KeyCode.A))//①Aキーがおされたら 
         {
+            anim.SetBool("Run", true);
             //左にMoveSpeed＊Time.deltaTimeだけ動かす
             characterController.Move(this.gameObject.transform.right * -1 * MoveSpeed * Time.deltaTime);
+        }
+        else if(Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetBool("Run", false);
         }
 
         if (Input.GetKey(KeyCode.D))//①Dキーがおされたら 
         {
+            anim.SetBool("Run", true);
             //右にMoveSpeed＊Time.deltaTimeだけ動かす
             characterController.Move(this.gameObject.transform.right * MoveSpeed * Time.deltaTime);
         }
-
+        else if(Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetBool("Run", false);
+        }
 
         characterController.Move(Velocity);//キャラクターコントローラーをVeloctiyだけ動かし続ける
         Velocity.y += Physics.gravity.y * Time.deltaTime;   //Velocityのy軸を重力*Time.deltaTime分だけ動かす
@@ -84,6 +88,21 @@ public class PlayerControl : MonoBehaviour
             if(Input.GetKeyDown(KeyCode.Space)) //スペースキーが押されたら
             {
                 Velocity.y = JumpPowewr;    //Velocity.y を JumpPowewrにする
+            }
+        }
+
+        if (Input.GetMouseButton(0))//マウスが左クリックされたら
+        {
+            Ray ray = new Ray(Muzzle.transform.position, Muzzle.transform.forward); //RayをMuzzleの場所から前方に飛ばす
+            Debug.DrawRay(ray.origin, ray.direction, Color.red);    //Rayを赤色で表示させる
+
+            if(Physics.Raycast(ray,out hit,distance))   //Rayがdistanceの範囲内で何かに当たった時に
+            {
+                if(hit.collider.tag == "Enemy") //もし当たった物のタグがEnemyだったら
+                {
+                    Destroy(hit.collider.gameObject);   //当たった物を消去して
+                    Instantiate(Explosion.gameObject, hit.collider.gameObject.transform.position, gameObject.transform.rotation);//Rayが当たった場所に爆発を生成する
+                }
             }
         }
     }
